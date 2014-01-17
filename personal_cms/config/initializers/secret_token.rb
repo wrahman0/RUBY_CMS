5cfb3eb9bef6831d2e52e4912153476f5ace0797
Wasiur Rahman <wrahman0@gmail.com>
2014-01-17 16:15:31 -0500

    Initial Commit
    Start of a very long project :D

---
+++ personal_cms/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PersonalCms::Application.config.secret_key_base = '1f40b976188cde0a45090b8859973b61361d11938e70db78a55c33a5b5bfa6ae8c109e7939a3a00f0760a4ee7aa315626868bdb80f7771bcad00bdb50a1c5726'
---
+++ personal_cms/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PersonalCms::Application.config.session_store :cookie_store, key: '_personal_cms_session'
